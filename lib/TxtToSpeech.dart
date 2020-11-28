8cff1125086bb22c4a98b0d3fc0e94820e1cf2b6
Bayarbileg <bayarbileg.batbileg@edu.unige.ch>
2020-11-28 22:35:01 +0100

    Bug : tts init not working

---
+++ lib/TxtToSpeech.dart
@@ -0,0 +1,251 @@
+import 'dart:async';
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+enum TtsState { playing, stopped, paused, continued }
+enum Direction_EN { upward, downward, right, left }
+
+class TxtToSpeech {
+  FlutterTts flutterTts;
+  dynamic languages;
+  String language = "en-US";
+  double volume = 1.0;
+  double pitch = 1.0;
+  double rate = 1.0;
+
+  String _newVoiceText;
+
+  TtsState ttsState = TtsState.stopped;
+
+  get isPlaying => ttsState == TtsState.playing;
+
+  get isStopped => ttsState == TtsState.stopped;
+
+  get isPaused => ttsState == TtsState.paused;
+
+  get isContinued => ttsState == TtsState.continued;
+
+  TxtToSpeech();
+
+  initTts() async {
+    this.flutterTts = FlutterTts();
+
+    _getLanguages();
+
+    if (!kIsWeb) {
+      if (Platform.isAndroid) {
+        _getEngines();
+      }
+    }
+
+    flutterTts.setStartHandler(() {
+      print("Playing");
+      ttsState = TtsState.playing;
+    });
+
+    flutterTts.setCompletionHandler(() {
+      print("Complete");
+      ttsState = TtsState.stopped;
+    });
+
+    flutterTts.setCancelHandler(() {
+      print("Cancel");
+      ttsState = TtsState.stopped;
+    });
+
+    if (kIsWeb || Platform.isIOS) {
+      flutterTts.setPauseHandler(() {
+        print("Paused");
+        ttsState = TtsState.paused;
+      });
+
+      flutterTts.setContinueHandler(() {
+        print("Continued");
+        ttsState = TtsState.continued;
+      });
+    }
+
+    flutterTts.setErrorHandler((msg) {
+      print("error: $msg");
+      ttsState = TtsState.stopped;
+    });
+  }
+
+  Future _getLanguages() {
+    if (languages != null){ 
+
+      languages = flutterTts.getLanguages;
+    }
+  }
+
+  Future _getEngines() async {
+    var engines = await flutterTts.getEngines;
+    if (engines != null) {
+      for (dynamic engine in engines) {
+        print(engine);
+      }
+    }
+  }
+
+  Future speak(String s) async {
+    await flutterTts.setVolume(volume);
+    await flutterTts.setSpeechRate(rate);
+    await flutterTts.setPitch(pitch);
+
+    if (_newVoiceText != null) {
+      if (_newVoiceText.isNotEmpty) {
+        await flutterTts.awaitSpeakCompletion(true);
+        await flutterTts.speak(s);
+      }
+    }
+  }
+
+  Future _stop() async {
+    var result = await flutterTts.stop();
+    if (result == 1) ttsState = TtsState.stopped;
+  }
+
+  Future _pause() async {
+    var result = await flutterTts.pause();
+    if (result == 1) ttsState = TtsState.paused;
+  }
+
+  @override
+  void dispose() {
+    flutterTts.stop();
+  }
+
+  List<DropdownMenuItem<String>> getLanguageDropDownMenuItems() {
+    var items = List<DropdownMenuItem<String>>();
+    for (dynamic type in languages) {
+      items.add(
+          DropdownMenuItem(value: type as String, child: Text(type as String)));
+    }
+    return items;
+  }
+
+  void changedLanguageDropDownItem(String selectedType) {
+    language = selectedType;
+    flutterTts.setLanguage(language);
+  }
+
+  void _onChange(String text) {
+    _newVoiceText = text;
+  }
+
+  // Widget _inputSection() => Container(
+  //     alignment: Alignment.topCenter,
+  //     padding: EdgeInsets.only(top: 25.0, left: 25.0, right: 25.0),
+  //     child: TextField(
+  //       onChanged: (String value) {
+  //         _onChange(value);
+  //       },
+  //     ));
+
+  // Widget _btnSection() {
+  //   if (!kIsWeb && Platform.isAndroid) {
+  //     return Container(
+  //         padding: EdgeInsets.only(top: 50.0),
+  //         child:
+  //             Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+  //           _buildButtonColumn(Colors.green, Colors.greenAccent,
+  //               Icons.play_arrow, 'PLAY', _speak),
+  //           _buildButtonColumn(
+  //               Colors.red, Colors.redAccent, Icons.stop, 'STOP', _stop),
+  //         ]));
+  //   } else {
+  //     return Container(
+  //         padding: EdgeInsets.only(top: 50.0),
+  //         child:
+  //             Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+  //           _buildButtonColumn(Colors.green, Colors.greenAccent,
+  //               Icons.play_arrow, 'PLAY', _speak),
+  //           _buildButtonColumn(
+  //               Colors.red, Colors.redAccent, Icons.stop, 'STOP', _stop),
+  //           _buildButtonColumn(
+  //               Colors.blue, Colors.blueAccent, Icons.pause, 'PAUSE', _pause),
+  //         ]));
+  //   }
+  // }
+
+  Widget languageDropDownSection() {
+    return Container(
+        padding: EdgeInsets.only(top: 50.0),
+        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          DropdownButton(
+            value: language,
+            items: getLanguageDropDownMenuItems(),
+            onChanged: changedLanguageDropDownItem,
+          )
+        ]));
+  }
+  // Column _buildButtonColumn(Color color, Color splashColor, IconData icon,
+  //     String label, Function func) {
+  //   return Column(
+  //       mainAxisSize: MainAxisSize.min,
+  //       mainAxisAlignment: MainAxisAlignment.center,
+  //       children: [
+  //         IconButton(
+  //             icon: Icon(icon),
+  //             color: color,
+  //             splashColor: splashColor,
+  //             onPressed: () => func()),
+  //         Container(
+  //             margin: const EdgeInsets.only(top: 8.0),
+  //             child: Text(label,
+  //                 style: TextStyle(
+  //                     fontSize: 12.0,
+  //                     fontWeight: FontWeight.w400,
+  //                     color: color)))
+  //       ]);
+  // }
+
+  // Widget _buildSliders() {
+  //   return Column(
+  //     children: [_volume(), _pitch(), _rate()],
+  //   );
+  // }
+
+  // Widget _volume() {
+  //   return Slider(
+  //       value: volume,
+  //       onChanged: (newVolume) {
+  //         setState(() => volume = newVolume);
+  //       },
+  //       min: 0.0,
+  //       max: 1.0,
+  //       divisions: 10,
+  //       label: "Volume: $volume");
+  // }
+
+  // Widget _pitch() {
+  //   return Slider(
+  //     value: pitch,
+  //     onChanged: (newPitch) {
+  //       setState(() => pitch = newPitch);
+  //     },
+  //     min: 0.5,
+  //     max: 2.0,
+  //     divisions: 15,
+  //     label: "Pitch: $pitch",
+  //     activeColor: Colors.red,
+  //   );
+  // }
+
+  // Widget _rate() {
+  //   return Slider(
+  //     value: rate,
+  //     onChanged: (newRate) {
+  //       setState(() => rate = newRate);
+  //     },
+  //     min: 0.0,
+  //     max: 1.0,
+  //     divisions: 10,
+  //     label: "Rate: $rate",
+  //     activeColor: Colors.green,
+  //   );
+  // }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:camera/camera.dart';
 import 'package:image/image.dart' as imageLib;
+import 'package:tenji/TxtToSpeech.dart';
 import 'package:tflite/tflite.dart';
 
 import 'RectPainter.dart';
@@ -25,146 +26,27 @@ class TenjiHomePage extends StatefulWidget {
 }
 
 class _TenjiHomePageState extends State<TenjiHomePage> {
-  CameraController _controller;
-  bool _cameraInitialized = false;
-  String _topText = '';
-
-  bool _isDetecting = false;
-
-  var _savedRect;
+  TxtToSpeech tts; 
 
   @override
   void initState() {
     super.initState();
-    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
-    _initializeCamera();
-  }
-
-  void _initializeCamera() async {
-    _topText = 'Waiting for camera initialization\n';
-    await Tflite.loadModel(
-        model: "assets/model.tflite", labels: "assets/labels.txt");
-
-    List<CameraDescription> cameras = await availableCameras();
-    _controller = CameraController(cameras[0], ResolutionPreset.medium);
-    _controller.initialize().then((_) async {
-      _cameraInitialized = true;
-      await _controller
-          .startImageStream((CameraImage image) => _processCameraImage(image));
-      _topText = 'Camera Initialization complete\n';
-      setState(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    _controller?.dispose();
-    super.dispose();
+    tts = new TxtToSpeech();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: Container(
           color: Colors.black,
-          child: Column(
+          child: Row(
             children: <Widget>[
-              Text(
-                _topText,
-                style: TextStyle(color: Colors.white, fontSize: 18),
-              ),
-              _cameraInitialized
-                  ? Expanded(
-                      child: OverflowBox(
-                          maxWidth: double.infinity,
-                          child: AspectRatio(
-                            aspectRatio: _controller.value.aspectRatio,
-                            child: Stack(
-                                fit: StackFit.expand,
-                                children: <Widget>[
-                                  CameraPreview(_controller),
-                                  CustomPaint(painter: RectPainter(_savedRect))
-                                ]),
-                          )))
-                  : Container(),
+              tts.languages != null ? tts.languageDropDownSection() : Container(),
+              FloatingActionButton(onPressed: () async => await tts.speak('upward')),
+
             ],
           )),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: print("not yet implemented"),
-      //   backgroundColor: Colors.white,
-      // ),
     );
-  }
-
-  void _processCameraImage(CameraImage image) async {
-    if (_isDetecting) return;
-    _isDetecting = true;
-    Future findObjectFuture = _findObject(image);
-    List results = await Future.wait(
-        [findObjectFuture, Future.delayed(Duration(milliseconds: 500))]);
-    // Detecting object is done here.
-    setState(() {
-      _savedRect = results[0];
-    });
-    _isDetecting = false;
-  }
-
-  static imageLib.Image _convertCameraImage(CameraImage image) {
-    int width = image.width;
-    int height = image.height;
-    // imglib -> Image package from https://pub.dartlang.org/packages/image
-    var img = imageLib.Image(width, height); // Create Image buffer
-    const int hexFF = 0xFF000000;
-    final int uvyButtonStride = image.planes[1].bytesPerRow;
-    final int uvPixelStride = image.planes[1].bytesPerPixel;
-    for (int x = 0; x < width; x++) {
-      for (int y = 0; y < height; y++) {
-        final int uvIndex =
-            uvPixelStride * (x / 2).floor() + uvyButtonStride * (y / 2).floor();
-        final int index = y * width + x;
-        final yp = image.planes[0].bytes[index];
-        final up = image.planes[1].bytes[uvIndex];
-        final vp = image.planes[2].bytes[uvIndex];
-        // Calculate pixel color
-        int r = (yp + vp * 1436 / 1024 - 179).round().clamp(0, 255);
-        int g = (yp - up * 46549 / 131072 + 44 - vp * 93604 / 131072 + 91)
-            .round()
-            .clamp(0, 255);
-        int b = (yp + up * 1814 / 1024 - 227).round().clamp(0, 255);
-        // color: 0x FF  FF  FF  FF
-        //           A   B   G   R
-        img.data[index] = hexFF | (b << 16) | (g << 8) | r;
-      }
-    }
-    // Rotate 90 degrees to upright
-    var img1 = imageLib.copyRotate(img, 90);
-    return img1;
-  }
-
-  Future<Map> _findObject(CameraImage image) async {
-    List resultList = await Tflite.detectObjectOnFrame(
-      bytesList: image.planes.map((plane) {
-        return plane.bytes;
-      }).toList(),
-      model: "SBB_BUTTON_MODEL",
-      // TODO: Check whether necessary to resize image like in the SBB example in python.
-      imageHeight: image.height,
-      imageWidth: image.width,
-      // TODO: Check other parameters necessary
-    );
-
-    // TODO:  FIND AN ALTERNATIVE TO RETURNING THE BIGGEST MATCH SHOULD RETURN DOOR OR BUTTON
-    Map biggetsRect;
-    double rectSize, rectMax = 0.0;
-    for (int i = 0; i < resultList.length; i++) {
-      Map aRect = resultList[i]["rect"];
-      rectSize = aRect["w"] * aRect["h"];
-      if (rectSize > rectMax) {
-        rectMax = rectSize;
-        biggetsRect = aRect;
-      }
-    }
-    return biggetsRect;
   }
 }
